e2d2e1a9dede236664505482bdc32a4390b15e1a
Manoj Kumar <napestershine@gmail.com>
2023-01-04 19:53:06 +0100

    added more pages

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,3 +1,25 @@
 <template>
-  <h1>Contct us</h1>
+  <div>
+    <h1>Contct us</h1>
+    <form>
+      <div class="mb-3">
+        <label for="exampleInputEmail1" class="form-label">Email address</label>
+        <input id="exampleInputEmail1" type="email" class="form-control" aria-describedby="emailHelp">
+        <div id="emailHelp" class="form-text">
+          We'll never share your email with anyone else.
+        </div>
+      </div>
+      <div class="mb-3">
+        <label for="exampleInputPassword1" class="form-label">Password</label>
+        <input id="exampleInputPassword1" type="password" class="form-control">
+      </div>
+      <div class="mb-3 form-check">
+        <input id="exampleCheck1" type="checkbox" class="form-check-input">
+        <label class="form-check-label" for="exampleCheck1">Check me out</label>
+      </div>
+      <button type="submit" class="btn btn-primary">
+        Submit
+      </button>
+    </form>
+  </div>
 </template>
